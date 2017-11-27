fbc20be8d6805032b20932f0f2e15b77844e1046
Mike Grusin <mike.grusin+github@sparkfun.com>
2017-11-27 16:56:02 -0700

    updated variable names

--- LPP_3_CustomColorMixing/LPP_3_CustomColorMixing.ino
+++ LPP_3_CustomColorMixing/LPP_3_CustomColorMixing.ino
@@ -19,26 +19,26 @@ This code is released under the MIT License (http://opensource.org/licenses/MIT)
 
 // The built-in LED:
 
-int RGBredpin = 12;
-int RGBgreenpin = 13;
-int RGBbluepin = 14;
+int RGB_red = 12;
+int RGB_green = 13;
+int RGB_blue = 14;
 
 // The colored LEDs along the bottom edge of the board:
 
-int redpin = 6;
-int greenpin = A7;
-int bluepin = A8;
+int redLED = 6;
+int greenLED = A7;
+int blueLED = A8;
 
 void setup() {
 
 // Make all of our LED pins outputs:
 
-  pinMode(RGBredpin, OUTPUT);
-  pinMode(RGBgreenpin, OUTPUT);
-  pinMode(RGBbluepin, OUTPUT);
-  pinMode(redpin, OUTPUT);
-  pinMode(greenpin, OUTPUT);
-  pinMode(bluepin, OUTPUT);
+  pinMode(RGB_red, OUTPUT);
+  pinMode(RGB_green, OUTPUT);
+  pinMode(RGB_blue, OUTPUT);
+  pinMode(redLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+  pinMode(blueLED, OUTPUT);
 }
 
 void loop()
@@ -56,134 +56,134 @@ void loop()
   
   // Red
 
-  analogWrite(RGBredpin,255);
-  analogWrite(RGBgreenpin,0);
-  analogWrite(RGBbluepin,0);
+  analogWrite(RGB_red,255);
+  analogWrite(RGB_green,0);
+  analogWrite(RGB_blue,0);
 
-  analogWrite(redpin,255);
-  analogWrite(greenpin,0);
-  analogWrite(bluepin,0);
+  analogWrite(redLED,255);
+  analogWrite(greenLED,0);
+  analogWrite(blueLED,0);
   delay(1000);
 
   // Orange
 
-  analogWrite(RGBredpin,255);
-  analogWrite(RGBgreenpin,128);
-  analogWrite(RGBbluepin,0);
+  analogWrite(RGB_red,255);
+  analogWrite(RGB_green,128);
+  analogWrite(RGB_blue,0);
 
-  analogWrite(redpin,255);
-  analogWrite(greenpin,128);
-  analogWrite(bluepin,0);
+  analogWrite(redLED,255);
+  analogWrite(greenLED,128);
+  analogWrite(blueLED,0);
   delay(1000);
 
   // Yellow
 
-  analogWrite(RGBredpin,255);
-  analogWrite(RGBgreenpin,255);
-  analogWrite(RGBbluepin,0);
+  analogWrite(RGB_red,255);
+  analogWrite(RGB_green,255);
+  analogWrite(RGB_blue,0);
 
-  analogWrite(redpin,255);
-  analogWrite(greenpin,255);
-  analogWrite(bluepin,0);
+  analogWrite(redLED,255);
+  analogWrite(greenLED,255);
+  analogWrite(blueLED,0);
   delay(1000);
 
   // Chartruese
 
-  analogWrite(RGBredpin,128);
-  analogWrite(RGBgreenpin,255);
-  analogWrite(RGBbluepin,0);
+  analogWrite(RGB_red,128);
+  analogWrite(RGB_green,255);
+  analogWrite(RGB_blue,0);
 
-  analogWrite(redpin,128);
-  analogWrite(greenpin,255);
-  analogWrite(bluepin,0);
+  analogWrite(redLED,128);
+  analogWrite(greenLED,255);
+  analogWrite(blueLED,0);
   delay(1000);
 
   // Green
 
-  analogWrite(RGBredpin,0);
-  analogWrite(RGBgreenpin,255);
-  analogWrite(RGBbluepin,0);
+  analogWrite(RGB_red,0);
+  analogWrite(RGB_green,255);
+  analogWrite(RGB_blue,0);
 
-  analogWrite(redpin,0);
-  analogWrite(greenpin,255);
-  analogWrite(bluepin,0);
+  analogWrite(redLED,0);
+  analogWrite(greenLED,255);
+  analogWrite(blueLED,0);
   delay(1000);
 
   // Spring Green
 
-  analogWrite(RGBredpin,0);
-  analogWrite(RGBgreenpin,255);
-  analogWrite(RGBbluepin,128);
+  analogWrite(RGB_red,0);
+  analogWrite(RGB_green,255);
+  analogWrite(RGB_blue,128);
 
-  analogWrite(redpin,0);
-  analogWrite(greenpin,255);
-  analogWrite(bluepin,128);
+  analogWrite(redLED,0);
+  analogWrite(greenLED,255);
+  analogWrite(blueLED,128);
   delay(1000);
 
   // Cyan
 
-  analogWrite(RGBredpin,0);
-  analogWrite(RGBgreenpin,255);
-  analogWrite(RGBbluepin,255);
+  analogWrite(RGB_red,0);
+  analogWrite(RGB_green,255);
+  analogWrite(RGB_blue,255);
 
-  analogWrite(redpin,0);
-  analogWrite(greenpin,255);
-  analogWrite(bluepin,255);
+  analogWrite(redLED,0);
+  analogWrite(greenLED,255);
+  analogWrite(blueLED,255);
   delay(1000);
 
   // Azure
 
-  analogWrite(RGBredpin,0);
-  analogWrite(RGBgreenpin,128);
-  analogWrite(RGBbluepin,255);
+  analogWrite(RGB_red,0);
+  analogWrite(RGB_green,128);
+  analogWrite(RGB_blue,255);
 
-  analogWrite(redpin,0);
-  analogWrite(greenpin,128);
-  analogWrite(bluepin,255);
+  analogWrite(redLED,0);
+  analogWrite(greenLED,128);
+  analogWrite(blueLED,255);
   delay(1000);
 
   // Blue
 
-  analogWrite(RGBredpin,0);
-  analogWrite(RGBgreenpin,0);
-  analogWrite(RGBbluepin,255);
+  analogWrite(RGB_red,0);
+  analogWrite(RGB_green,0);
+  analogWrite(RGB_blue,255);
 
-  analogWrite(redpin,0);
-  analogWrite(greenpin,0);
-  analogWrite(bluepin,255);
+  analogWrite(redLED,0);
+  analogWrite(greenLED,0);
+  analogWrite(blueLED,255);
   delay(1000);
 
   // Violet
 
-  analogWrite(RGBredpin,128);
-  analogWrite(RGBgreenpin,0);
-  analogWrite(RGBbluepin,255);
+  analogWrite(RGB_red,128);
+  analogWrite(RGB_green,0);
+  analogWrite(RGB_blue,255);
 
-  analogWrite(redpin,128);
-  analogWrite(greenpin,0);
-  analogWrite(bluepin,255);
+  analogWrite(redLED,128);
+  analogWrite(greenLED,0);
+  analogWrite(blueLED,255);
   delay(1000);
 
   // Magenta
   
-  analogWrite(RGBredpin,255);
-  analogWrite(RGBgreenpin,0);
-  analogWrite(RGBbluepin,255);
+  analogWrite(RGB_red,255);
+  analogWrite(RGB_green,0);
+  analogWrite(RGB_blue,255);
 
-  analogWrite(redpin,255);
-  analogWrite(greenpin,0);
-  analogWrite(bluepin,255);
+  analogWrite(redLED,255);
+  analogWrite(greenLED,0);
+  analogWrite(blueLED,255);
   delay(1000);
 
   // Rose
   
-  analogWrite(RGBredpin,255);
-  analogWrite(RGBgreenpin,0);
-  analogWrite(RGBbluepin,128);
+  analogWrite(RGB_red,255);
+  analogWrite(RGB_green,0);
+  analogWrite(RGB_blue,128);
   
-  analogWrite(redpin,255);
-  analogWrite(greenpin,0);
-  analogWrite(bluepin,128);
+  analogWrite(redLED,255);
+  analogWrite(greenLED,0);
+  analogWrite(blueLED,128);
   delay(1000);
 
 }
